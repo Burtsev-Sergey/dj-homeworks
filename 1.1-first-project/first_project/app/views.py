d8fec7f0d5885b43ced9fbc21a6b1a9ac5dd55c7
Burtsev Sergey <sburtsevs@mail.ru>
2025-02-15 22:40:13 +0300

    Updating comments in views.py

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -8,27 +8,26 @@ from django.shortcuts import render, reverse
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+    
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
     
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
     return render(request, template_name, context)
 
 
+# Функция формирует и передает текущее время
 def time_view(request):
     current_time = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
     return HttpResponse(f'<strong>Текущее время:</strong> {current_time}')
 
-    
+
+ # Функция формирует и передает список файлов в рабочей директории   
 def workdir_view(request):
     try:
         dir_list = '<br>'.join(os.listdir(os.getcwd()))
